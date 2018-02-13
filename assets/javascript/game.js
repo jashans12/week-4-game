2c3f36fe861f4e2b2412bb037f01247d8f8288f5
jashans12 <hansenjason12@gmail.com>
2018-02-12 18:19:58 -0700

    Crystals work now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -62,19 +62,19 @@ var whiteCrystal = $("<img>");
 
 //experiment start
 
-$(".crystal-image").on("click", function() {
+$("#crystals").on("click", ".crystal-image", function() {
     // Determining the crystal's value requires us to extract the value from the data attribute.
     // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
     // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
     // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
-    
+    console.log("test");
     var crystalValue = ($(this).attr("data-crystalvalue"));
     crystalValue = parseInt(crystalValue);
     // We then add the crystalValue to the user's "counter" which is a global variable.
     // Every click, from every crystal adds to the global counter.
     total += crystalValue;
     // All of the same game win-lose logic applies. So the rest remains unchanged.
-    alert("New score: " + total);
+    $('#score').text(total);
     if (total === targetNumber) {
         alert("You win!");
       }
